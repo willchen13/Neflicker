ee5c841b1f63b6033a3d68eaad217d81dc0f0a46
William Chen <williamchen13225@gmail.com>
2021-04-06 18:44:22 -0700

    created Jumbotron component and rendered stored data with compound components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Jumbotron from './components/jumbotron';
+import jumboData from './fixtures/jumbo.json';
 
-function App() {
+export default function App() {
   return (
-    <p> hello </p>
+    <Jumbotron.Container>
+      {jumboData.map(item => (
+        <Jumbotron key={item.id} direction={item.direction} >
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+          </Jumbotron.Pane>
+
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}/>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+
+    </Jumbotron.Container>
   );
 }
-
-export default App;
